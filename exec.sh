40c2139bad5bfc95d0b415dcf0d10e3961d0d3cb
tholulomo <fatexjames@yahoo.com>
2022-03-28 23:49:23 -0400

    Updated execution script to pull and run

--- exec.sh
+++ exec.sh
@@ -1,21 +1,39 @@
 #/bin/bash
 
+if [[ $(whoami) != "root" ]]; then
+  echo 'please run this script as root'
+  exit
+fi
+
 gpg --quiet --batch --yes --decrypt --passphrase='$passphrase' --output ./ENV ENV.gpg 2> /dev/null
+
+alias composePullUp="docker-compose pull && docker-compose up -d"
 
 if [ ! -f ./ENV ]; then
     echo "File does not exist"
+    exit
 else 
     source ./ENV
 
-    echo add user ${APP_USER}
-    useradd --home-dir /${APP_DIR} --shell /bin/bash -G sudo -M -U ${APP_USER}
+    if [[ -z "${APP_USER}" ]]; then
+        exit
+    fi
 
-    if [[ ! -d /apps ]]; then
-        (su root -c 'mkdir /${APP_DIR}; touch /${APP_DIR}/ENV; chown -R ${APP_USER}:${APP_USER} /${APP_DIR}; ls -lasR /${APP_DIR}')
+    if [ id -u "${APP_USER}" >/dev/null 2>&1 ]; then
+        echo "user exists"
+    else
+        echo "add user ${APP_USER}"
+        useradd --home-dir /${APP_DIR} --shell /bin/bash -G sudo -M -U ${APP_USER}
+    fi
+
+    if [[ ! -d /${APP_DIR} ]]; then
+        (su root -c "mkdir /${APP_DIR}; touch /${APP_DIR}/ENV; chown -R ${APP_USER}:${APP_USER} /${APP_DIR}; ls -lasR /${APP_DIR}")
     fi
 
     cd /${APP_DIR}
 
     # Pull latest docker
     git clone git@github.com:Duke-MatSci/deployer.git .
+
+    composePullUp
 fi
